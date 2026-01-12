b91404074fb1c83dee31caf8b76525eb92d5d169
Heba Shalayel <hebakhshalayel@gmail.com>
2026-01-12 14:01:58 +0200

    Update main.js

--- javascript/main.js
+++ javascript/main.js
@@ -16,7 +16,6 @@
     if (!selectHeader.classList.contains('scroll-up-sticky') && !selectHeader.classList.contains('sticky-top') && !selectHeader.classList.contains('fixed-top')) return;
     window.scrollY > 100 ? selectBody.classList.add('scrolled') : selectBody.classList.remove('scrolled');
   }
-
   
   
   document.addEventListener('scroll', toggleScrolled);
